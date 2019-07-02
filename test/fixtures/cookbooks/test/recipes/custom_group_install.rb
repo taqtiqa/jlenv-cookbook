3120fc360bda26eeb25d2514afbf4c6ea778ed87
taqtiqa-admin <admin@taqtiqa.com>
2019-07-02 15:25:43 +1000

    Port group install for jlenv

--- test/fixtures/cookbooks/test/recipes/custom_group_install.rb
+++ test/fixtures/cookbooks/test/recipes/custom_group_install.rb
@@ -1,5 +1,5 @@
-# Install rbenv and makes it avilable to the selected user
-version = '2.4.1'
+# Install jlenv and makes it avilable to the selected user
+version = '1.0.1'
 
 # Make sure that Vagarant user is on the box for dokken
 include_recipe 'test::dokken'
@@ -8,21 +8,22 @@ group 'new-group' do
   members 'vagrant'
 end
 
-# Keeps the rbenv install upto date
-rbenv_user_install 'vagrant' do
+# Keeps the jlenv install upto date
+jlenv_user_install 'vagrant' do
   user 'vagrant'
   group 'new-group'
 end
 
-rbenv_plugin 'ruby-build' do
-  git_url 'https://github.com/rbenv/ruby-build.git'
+# Install plugin to build Julia
+jlenv_plugin 'julia-build' do
+  git_url 'https://github.com/HiroakiMikami/julia-build.git'
   user 'vagrant'
 end
 
-rbenv_ruby '2.4.1' do
+jlenv_ruby '1.0.1' do
   user 'vagrant'
 end
 
-rbenv_global version do
+jlenv_global version do
   user 'vagrant'
 end
